87e38a42c67910fb8043db9b8a97447c0e6a29ca
Jaromir Obr <jaromir.obr@gmail.com>
2022-06-21 21:12:08 +0200

    Change cli to accept JSON input, see https://vino.dev/blog/node-to-rust-day-22-using-json/

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use log::{debug, error, info};
 use my_lib::Module;
@@ -13,19 +13,24 @@ use structopt::{clap::AppSettings, StructOpt};
     ]),
 )]
 struct CliOptions {
-    /// The WebAssembly file to load, try "crates/my-lib/tests/wapc_guest.wasm"
+    /// The WebAssembly file to load, try "crates/my-lib/tests/wapc_guest.wasm" or "./blog.wasm"
     #[structopt(parse(from_os_str))]
     pub(crate) file_path: PathBuf,
 
-    /// The operation to invoke in the WASM file, try "hello".
+    /// The operation to invoke in the WASM file, try "hello" or "render"
     #[structopt()]
     pub(crate) operation: String,
 
-    /// The data to pass to the operation
-    #[structopt()]
-    pub(crate) data: String,
+    /// The path to the JSON data to use as input. Try "./hello.json" or "./blog.json"
+    #[structopt(parse(from_os_str))]
+    pub(crate) json_path: PathBuf,
 }
 
+/// Usage:
+/// ```bash
+/// $ cargo run crates/my-lib/tests/wapc_guest.wasm hello hello.json
+/// $ cargo run -r ./blog.wasm render ./blog.json # Release build
+/// ```
 fn main() {
     env_logger::init();
     debug!("Initialized logger");
@@ -43,11 +48,16 @@ fn main() {
     };
 }
 
-fn run(options: CliOptions) -> anyhow::Result<String> {
+fn run(options: CliOptions) -> anyhow::Result<serde_json::Value> {
     let module = Module::from_file(&options.file_path)?;
     info!("Module loaded");
 
-    let bytes = rmp_serde::to_vec(&options.data)?;
+    let json = fs::read_to_string(options.json_path)?;
+    let data: serde_json::Value = serde_json::from_str(&json)?;
+    debug!("Data: {:?}", data);
+    let bytes = rmp_serde::to_vec(&data)?;
+
+    debug!("Running  {} with payload: {:?}", options.operation, bytes);
     let result = module.run(&options.operation, &bytes)?;
     let unpacked = rmp_serde::from_slice(&result)?;
 
